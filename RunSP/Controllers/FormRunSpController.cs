3ce267922fcae4e7d84588de6ff552baa2c47c1b
eadgmk <ahmad.fauzi@gandummas.co.id>
2017-10-05 17:32:34 +0700

    commit 2

--- RunSP/Controllers/FormRunSpController.cs
+++ RunSP/Controllers/FormRunSpController.cs
@@ -6,40 +6,59 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data;
 using System.Configuration;
+using System.Web.Security;
+using System.Data.SqlClient;
+using RunSP.Models;
 
 namespace RunSP.Controllers
 {
     public class FormRunSpController : Controller
     {
 
-       
-
-            
+                         
         //
         // GET: /FormRunSp/
-        public void Index()
+        public ActionResult Index(Barang model)
         {
-            Dataset ds = new Dataset();
+            //buttonclick();
+            if (!String.IsNullOrEmpty(model.btnsubmit))
+            {
+                Dataset ds = new Dataset();
+                SqlConnection conn;
+                SqlCommand cmd = new SqlCommand();
+
+                SqlParameter sp1 = new SqlParameter();
+                SqlParameter sp2 = new SqlParameter();
+
+                conn = new SqlConnection("server=10.10.10.26:database=DbTest;uid=sa;pwd=Haan@Colatta138");
+                cmd.Parameters.Add("@nama", sqlDbType.Varchar).Value = fromdate.text;
+                cmd.Parameters.Add("@jenis", sqlDbType.Varchar).Value = todate.text;
+                cmd = new SqlCommand("submitrecord", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return View();
+        }
+
+        protected void buttonclick(object sender, EventArgs e)
+        {
+           /* Dataset ds = new Dataset();
             SqlConnection conn;
             SqlCommand cmd = new SqlCommand();
 
             SqlParameter sp1 = new SqlParameter();
             SqlParameter sp2 = new SqlParameter();
 
-            conn = new SqlConnection("server=10.10.10.212:database=AXGMKDW;uid=sa;pwd=Haan@123");
-            cmd.Parameters.Add("@fromdate", sqlDbType.Varchar).Value = fromdate.text;
-            cmd.Parameters.Add("@todate", sqlDbType.Varchar).Value = todate.text;
+            conn = new SqlConnection("server=10.10.10.26:database=DbTest;uid=sa;pwd=Haan@Colatta138");
+            cmd.Parameters.Add("@nama", sqlDbType.Varchar).Value = fromdate.text;
+            cmd.Parameters.Add("@jenis", sqlDbType.Varchar).Value = todate.text;
             cmd = new SqlCommand("submitrecord", conn);
             cmd.CommandType = CommandType.StoredProcedure;
             conn.Open();
             cmd.ExecuteNonQuery();
-            conn.Close();
-            //return View();
-        }
-
-        protected void buttonclick(object sender, EventArgs e)
-        {
-            
+            conn.Close();*/
         }
 
         public ActionResult Welcome(string name, int numTimes = 1)
---
+++ RunSP/Models/Barang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RunSP.Models
+{
+    public class Barang
+    {
+        public DateTime Nama { get; set; }
+
+        public String Jenis { get; set; }
+    }
+}
